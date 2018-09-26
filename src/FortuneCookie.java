fd00cba9c11a6fddea5c94c790ae5c09e9aa76cc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-26 16:57:18 -0700

    Jack, Fortune, Start of Sound Effect

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,30 +1,53 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener{
 	
 	public void showButton() {
 	     System.out.println("Button clicked");
+	     JFrame frame = new JFrame();
+			frame.setVisible(true);
+			
+			JButton button = new JButton();
+			frame.add(button); 
+			
+			button.setText("Open Cookie!");
+			button.addActionListener(this);
+			
 	}
+	
 	public static void main(String[] args) {
 		FortuneCookie fc = new FortuneCookie();
-		fc.showButton();
+		fc.showButton();	
 		
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		
-		JButton button = new JButton();
-		frame.add(button); 
-		
-		button.addActionListener(this);
+	
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		JOptionPane.showMessageDialog(null, "WooHoo");
+		int rand = new Random().nextInt(6);
+		if(rand == 1) {
+		JOptionPane.showMessageDialog(null, "You will break your leg");
+		}
+		else if(rand == 2) {
+			JOptionPane.showMessageDialog(null, "You will win something");
+			}
+		else if(rand == 3) {
+			JOptionPane.showMessageDialog(null, "You will get lucky");
+			}
+		else if(rand == 4) {
+			JOptionPane.showMessageDialog(null, "You will lose a friend");
+			}
+		else if(rand == 5) {
+			JOptionPane.showMessageDialog(null, "you will lose your code");
+			}
 	}
 
 
